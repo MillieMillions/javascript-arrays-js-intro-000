59b9cda9a080b19d637d0e134af1a2b85bb15ca2
Millie <milliealexblack@gmail.com>
2019-06-11 01:26:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat",]
 
-
-
-
-
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift("foo")
+  return array.unshift(element);
 }
